b04c1897f18e7f3badfe22a5d494ff4b1a369038
Sajid Mahmood <sajidm46@hotmail.com>
2022-08-04 03:34:50 +0100

    refactored

--- GitBuildTranlator.Test/TranslatorTest.cs
+++ GitBuildTranlator.Test/TranslatorTest.cs
@@ -11,22 +11,29 @@ namespace Tranlator.Tests;
 
 public class Tests
 {
-    ITranslatorService _service;
-    ITranslatorRepository _repository;
+    private TranslatorRepository _TranslatorService;
+    
     List<Tuple<string, string, string>> words;
-
+    private string eng1 = "";
+    private string french1 = "";
+    private string eng2 = "";
+    private string french2 = "";
+    private string eng3 = "";
+    private string french3 = "";
+    
     [SetUp]
     public void Setup()
     {
-        string eng1 = "Farnborough International Airshow, biennial global aerospace, defence and space trade event which showcases the latest commercial and military aircraft. Manufacturers such as Airbus and Boeing are expected to display their products and announce new orders * 2020 event was held virtually after the physical show was cancelled due to the coronavirus (COVID-19) pandemic";
-        string french1 = "Farnborough International Airshow, événement commercial biennal mondial de l'aérospatiale, de la défense et de l'espace qui présente les derniers avions commerciaux et militaires. Des fabricants tels qu'Airbus et Boeing devraient présenter leurs produits et annoncer de nouvelles commandes * L'événement 2020 a eu lieu pratiquement après l'annulation du salon physique en raison de la pandémie de coronavirus (COVID-19)";
+        _TranslatorService = new TranslatorRepository(new TranslatorService());
 
-        string eng2 = "Labour market statistics: integrated national release, including the latest data for employment, economic activity, economic inactivity, unemployment, claimant count, average earnings, productivity, unit wage costs, vacancies & labour disputes";
-        string french2 = "Statistiques sur le marché du travail : diffusion nationale intégrée, y compris les dernières données sur l'emploi, l'activité économique, l'inactivité économique, le chômage, le nombre de demandeurs, les gains moyens, la productivité, les coûts salariaux unitaires, les postes vacants";
+        eng1 = "Farnborough International Airshow, biennial global aerospace, defence and space trade event which showcases the latest commercial and military aircraft. Manufacturers such as Airbus and Boeing are expected to display their products and announce new orders * 2020 event was held virtually after the physical show was cancelled due to the coronavirus (COVID-19) pandemic";
+        french1 = "Farnborough International Airshow, événement commercial biennal mondial de l'aérospatiale, de la défense et de l'espace qui présente les derniers avions commerciaux et militaires. Des fabricants tels qu'Airbus et Boeing devraient présenter leurs produits et annoncer de nouvelles commandes * L'événement 2020 a eu lieu pratiquement après l'annulation du salon physique en raison de la pandémie de coronavirus (COVID-19)";
 
-        string eng3 = "City of London Corporation's Financial and Professional Services dinner. Chancellor Rishi Sunak and Bank of England Governor Andrew Bailey make their annual Mansion House speeches at the event hosted by the Lord Mayor of the City of London Vincent Keaveny";
-        string french3 = "Dîner des services financiers et professionnels de la City of London Corporation. Le chancelier Rishi Sunak et le gouverneur de la Banque d'Angleterre Andrew Bailey prononcent leurs discours annuels à Mansion House lors de l'événement organisé par le lord-maire de la ville de Londres Vincent Keaveny";
+        eng2 = "Labour market statistics: integrated national release, including the latest data for employment, economic activity, economic inactivity, unemployment, claimant count, average earnings, productivity, unit wage costs, vacancies & labour disputes";
+        french2 = "Statistiques sur le marché du travail : diffusion nationale intégrée, y compris les dernières données sur l'emploi, l'activité économique, l'inactivité économique, le chômage, le nombre de demandeurs, les gains moyens, la productivité, les coûts salariaux unitaires, les postes vacants";
 
+        eng3 = "City of London Corporation's Financial and Professional Services dinner. Chancellor Rishi Sunak and Bank of England Governor Andrew Bailey make their annual Mansion House speeches at the event hosted by the Lord Mayor of the City of London Vincent Keaveny";
+        french3 = "Dîner des services financiers et professionnels de la City of London Corporation. Le chancelier Rishi Sunak et le gouverneur de la Banque d'Angleterre Andrew Bailey prononcent leurs discours annuels à Mansion House lors de l'événement organisé par le lord-maire de la ville de Londres Vincent Keaveny";
 
         words = new List<Tuple<string, string, string>>();
         words.Add(new Tuple<string, string, string>("test words", "fr", "mots d'essai"));
@@ -37,15 +44,23 @@ public class Tests
 
 
     [Test]
-    public void Translate_TestWorld_MoteDessai()
+    public void Translate_Eng1_French1()
     {
-        var _TranslatorService = new TranslatorRepository(new TranslatorService());
+            var result = _TranslatorService.Translate(eng1, "fr");
+            Assert.AreEqual(french1, result);
+    }
 
-        string result = "";
-        foreach (var item in words)
-        {
-            result = _TranslatorService.Translate(item.Item1, item.Item2);
-            Assert.AreEqual(item.Item3, result);
-        }
+    [Test]
+    public void Translate_Eng2_French2()
+    {
+        var result = _TranslatorService.Translate(eng2, "fr");
+        Assert.AreEqual(french2, result);
+    }
+
+    [Test]
+    public void Translate_Eng3_French3()
+    {
+        var result = _TranslatorService.Translate(eng3, "fr");
+        Assert.AreEqual(french3, result);
     }
 }
